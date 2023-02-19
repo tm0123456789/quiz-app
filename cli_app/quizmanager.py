e6cde4a5ae26ece8c6b149873282173105b54329
tm0123456789 <toriomichelson@gmail.com>
2023-02-19 12:43:20 +0200

    Add quizmanager.py

---
+++ cli_app/quizmanager.py
@@ -0,0 +1,32 @@
+import os
+
+from quizparser import QuizParser
+
+
+class QuizManager:
+    def __init__(self, quizfolder):
+        self.quizfolder = quizfolder
+        self.the_quiz = None    # most recently selected quiz
+        self.quizzes = dict()   # collection of quizzes
+        self.results = None     # results of the most recent quiz
+        self.quiztaker = ''     # name of the person taking the quiz
+
+        if not os.path.exists(quizfolder):
+            raise FileNotFoundError('Quiz folder does not exist')
+        self._build_quiz_list()
+
+    def _build_quiz_list(self):
+        dircontents = os.scandir(self.quizfolder)
+        for i, f in enumerate(dircontents):
+            if f.is_file():
+                parser = QuizParser()
+                self.quizzes[i+1] = parser.parse_quiz(f)
+
+    def list_quizzes(self):
+        for k, v in self.quizzes.items():
+            print(f"[{k}]: {v.name}")
+
+
+# if __name__ == '__main__':
+#     qm = QuizManager('quizzes')
+#     qm.list_quizzes()
